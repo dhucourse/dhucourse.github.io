90d1f04a1f76dbbacf19f9a1aba72b3540392f57
jiachengdai <221310411@mail.dhu.edu.cn>
2025-11-25 01:07:01 +0800

    first commit

---
+++ assets/account-DJpGPDhY.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-CnmwwGkb.js";const n=e=>r.post("/account/register",e),o=e=>r.post("/account/login",e),c=()=>r.get("/account");export{c as g,o as l,n as r};
---
+++ assets/course-BPG2nUBP.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-CnmwwGkb.js";const o=e=>t.get("/course/getCourseListBySid?schoolId="+e),r=()=>t.get("/course/all"),u=e=>t.get("/course/getCodeById?cid="+e);export{o as a,u as b,r as g};
