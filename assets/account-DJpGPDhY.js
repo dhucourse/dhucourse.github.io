61ba235992003006883d59882447ecf6d24b6617
dhucourse <dhucourseshare@163.com>
2025-11-25 16:22:26 +0800

    Delete assets directory

--- assets/account-DJpGPDhY.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-CnmwwGkb.js";const n=e=>r.post("/account/register",e),o=e=>r.post("/account/login",e),c=()=>r.get("/account");export{c as g,o as l,n as r};
--- assets/course-BPG2nUBP.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./index-CnmwwGkb.js";const o=e=>t.get("/course/getCourseListBySid?schoolId="+e),r=()=>t.get("/course/all"),u=e=>t.get("/course/getCodeById?cid="+e);export{o as a,u as b,r as g};
